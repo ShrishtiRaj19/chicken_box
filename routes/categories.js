c8ad2d7c98a694f53499b6ba9c8e7ccba97635f7
shrishtiRaj19 <rajshrishti94@gmail.com>
2020-08-11 20:55:35 +0530

    routes issues

--- routes/categories.js
+++ routes/categories.js
@@ -1,13 +1,11 @@
 var categories = require('../controllers/categories');
 const validate = require('../controllers/users')
-console.log("validate", validate)
 
 module.exports = (router) => {
-	console.log("routereee")
     router.post('/categories/create',validate.authenticateToken, categories.createCategory);
     router.get('/categories/get', categories.getCategory);
     router.post('/categories/search', categories.searchCategory);
     router.get('/categories/:id', categories.getCategoryById);
     router.put('/categories/update/:id',validate.authenticateToken, categories.updateCategory);
     router.delete('/categories/delete/:id',validate.authenticateToken, categories.deleteCategory);
-},
+}
