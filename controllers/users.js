937a3574cfe865d5ccb946f93a13faf357bdddee
shrishtiRaj19 <rajshrishti94@gmail.com>
2020-08-15 12:48:00 +0530

    response json chsnges

--- controllers/users.js
+++ controllers/users.js
@@ -5,9 +5,7 @@ const jwt = require('jsonwebtoken')
 var ACCESS_TOKEN_SECRET='cd720ce3844bb9b8a628e8473c324fefa1f83567f30104cb511645df97d09eb79be8586b0a2ea9512a02ed04f886dde562b3ff8dcf2fc60e0610c9fe505f3dc0'
 
 const signup = async(req, res, next) =>{
-    
    try {
-    console.log("req.body?????????", req.body)
     const newUser = await userModel.create(req.body)
     return res.status(200).json(newUser)
   } catch (err) {
@@ -20,16 +18,16 @@ const signup = async(req, res, next) =>{
 const login = async(req, res, next) =>{
 	const logged_user = await userModel.findOne({email:req.body.email});
 	if (logged_user == null) 
-	  return res.send('Cannot find user')
+	  return res.json({status:"Failure", message:"Invalid email or password"})
 
 	const passwordexists = await userModel.findOne({password:req.body.password})   
 	  if(passwordexists==null) 
-		 return res.send('Not Allowed')
+		 return res.json({status:"Failure", message:"Invalid email or password"})
 		 
 	  //Authorize user
 	const user={email:req.body.email};
 	const accessToken=jwt.sign(user, ACCESS_TOKEN_SECRET) 
-	return res.json({accessToken:accessToken}) 
+	return res.json({status:"success", data:{access_token:accessToken}}) 
  
 }
 
@@ -37,7 +35,7 @@ const authenticateToken = (req, res, next) => {
 	const token = req.headers['access-token']
 	jwt.verify(token, ACCESS_TOKEN_SECRET, (err, user) => {
 	  console.log(err)
-	  if (err) return res.send({status:401, message:"unAuthorized User, please LoginIn"})
+	  if (err) return res.send({success:"Failure", message:"unAuthorized User, please LoginIn"})
 	  req.user = user
 	  next()
 	})
