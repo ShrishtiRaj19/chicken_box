d12e8368e1d1a6acfe6dc8dfaa9de712d88d8ff9
shrishtiRaj19 <rajshrishti94@gmail.com>
2020-09-01 20:00:05 +0530

    delete cart

--- routes/cart.js
+++ routes/cart.js
@@ -3,5 +3,5 @@ var cart = require('../controllers/cart');
 module.exports = (router) => {
     router.post('/cart/create', cart.createCart);
     router.get('/cart/get/:userId', cart.getCart);
-    router.delete('/cart/delete/:cartId', cart.deleteCart);
+    router.delete('/cart/delete', cart.deleteCart);
 }
