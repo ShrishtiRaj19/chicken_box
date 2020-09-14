e5c8d65b26aa3a94719495e471aa24d230f6ee52
rajshrishti94@gmail.com <shrishtiRaj19>
2020-09-13 18:40:16 -0700

    route changes

--- routes/payment.js
+++ routes/payment.js
@@ -2,7 +2,7 @@ var payment = require('../controllers/payment');
 
 module.exports = (router) => {
     router.post('/order', payment.order);
-    router.post('/capture/:paymentId', payment.payementDetails);
+    router.post('/capture/payement', payment.payementDetails);
 
   
 }
